d9757ea9534787bd13d9c8c6518508770b6ae3fa
jcrllano <arellanojuan033@gmail.com>
2025-10-16 15:33:50 -0500

    added new email send intent

--- CornerStore App/app/src/main/java/com/example/cornerstoreapp/CartList.java
+++ CornerStore App/app/src/main/java/com/example/cornerstoreapp/CartList.java
@@ -2,6 +2,7 @@ package com.example.cornerstoreapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -25,6 +26,25 @@ public class CartList extends AppCompatActivity {
         productList = (ArrayList<Product>) getIntent().getSerializableExtra("selected_products");
         productAdapter = new ProductAdapter(this, productList);
         recyclerView.setAdapter(productAdapter);
+
+        Button buttonEmail = findViewById(R.id.btnSendEmail);
+        buttonEmail.setOnClickListener(v -> sendEmail());
+    }
+
+    public void sendEmail() {
+        StringBuilder stringBuilder = new StringBuilder("Products List:\n\n");
+        for (Product product : productList) {
+            stringBuilder.append(product.getName());
+            stringBuilder.append(" $");
+            stringBuilder.append(product.getPrice());
+
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(android.net.Uri.parse("mailto:"));
+            intent.putExtra(intent.EXTRA_EMAIL, new String[]{"dolphinsnotredamfan@yahoo.com"});
+            intent.putExtra(intent.EXTRA_TEXT, stringBuilder.toString());
+
+            startActivity(intent);
+        }
     }
 
 }
