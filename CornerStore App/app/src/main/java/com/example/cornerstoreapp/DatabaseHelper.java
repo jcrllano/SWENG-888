8bd24d2414ac4b4e3738003bc0d7d82d1f8aed38
jcarllano <arellanojuan033@gmail.com>
2025-10-17 22:44:39 -0500

    Added comments to the DatabaseHelper class

--- CornerStore App/app/src/main/java/com/example/cornerstoreapp/DatabaseHelper.java
+++ CornerStore App/app/src/main/java/com/example/cornerstoreapp/DatabaseHelper.java
@@ -10,20 +10,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    //this is the name of the database
     public static final String DATABASE_NAME = "CornerStore.db";
+    //This is the version of database
     public static final Integer DATABASE_VERSION = 6;
+    //This is the table name
     public static final String DATABASE_TABLE_NAME = "products";
+    //This is the ID of the product
     public static final String DATABASE_COLUMN_ID = "id";
+    //This is the name of the product
     public static final String DATABASE_COLUMN_NAME = "name";
+    //This is the description of the product
     public static final String DATABASE_COLUMN_DESCRIPTION = "description";
+    //This is the name of the seller
     public static final String DATABASE_COLUMN_SELLER = "seller";
+    //This is the price of the product
     public static final String DATABASE_COLUMN_PRICE = "price";
+    //This is the iD of the product image
     public static final String DATABASE_COLUMN_PICTURE_ID = "pictureID";
 
-
+    //This the constrcutor of the class
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME , null, DATABASE_VERSION);
     }
+    //This method creates the table inside the database
     public static final String TABLE =
             "CREATE TABLE " + DATABASE_TABLE_NAME  + " (" +
                     DATABASE_COLUMN_ID + " INTEGER PRIMARY KEY, " +
@@ -33,18 +43,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     DATABASE_COLUMN_PRICE + " REAL, " +
                     DATABASE_COLUMN_PICTURE_ID + " INTEGER" +
                     ");";
+    //This method executes the SQL command to create the table
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE);
     }
 
+    //This method updates the table whenver there is a change on it
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE_NAME);
         onCreate(db);
     }
-
-
+    //This method add the products and puts them into the table
     public long addProduct(Product product) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -58,12 +69,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.close();
         return id;
     }
-
+    //This method gets all the products from the List arrays
     public List<Product> getAllProducts() {
         List<Product> productList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
+        //This queries the database
         Cursor cursor = db.rawQuery("SELECT * FROM " + DATABASE_TABLE_NAME, null);
-
+        //This maps the the database to be used in the product list
         if (cursor.moveToFirst()) {
             do {
                 Product product = new Product();
